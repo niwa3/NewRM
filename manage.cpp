ae23fedd8b072cd74756f3b3605e280707ffa91d
niwa <niwa@localhost.localdomain>
2017-10-02 13:24:34 +0900

    add relation manage funcs

--- manage.cpp
+++ manage.cpp
@@ -1,7 +1,7 @@
 #include "manage.h"
 
-//==========LoginManager===========
-LoginManager::LoginManager(std::string filename)
+//==========LoginManageFuncs===========
+LoginManageFuncs::LoginManageFuncs(std::string filename)
 {
   std::ifstream db_conf(filename.c_str(), std::ios::in);
   std::string reading_line_buffer;
@@ -29,7 +29,7 @@ LoginManager::LoginManager(std::string filename)
   l_dao.reset(new LoginInfoDao(dbname,user,password));
 }
 
-bool LoginManager::register_Linfo(
+bool LoginManageFuncs::register_Linfo(
     std::string login,
     std::string password,
     USERTYPE user_type)
@@ -42,26 +42,28 @@ bool LoginManager::register_Linfo(
   else return false;
 }
 
-LoginInfo LoginManager::fetch_Linfo_by_login(std::string login)
+LoginInfo LoginManageFuncs::fetch_Linfo_by_login(std::string login)
 {
   LoginInfo login_info_from_db = LoginInfo();
   if (l_dao->fetch("login='"+login+"'", login_info_from_db)){return login_info_from_db;}
   else return login_info_from_db;
 }
 
-LoginInfo LoginManager::fetch_Linfo_by_l_id(int l_id)
+LoginInfo LoginManageFuncs::fetch_Linfo_by_l_id(int l_id)
 {
   LoginInfo login_info_from_db = LoginInfo();
   if (l_dao->fetch("id="+std::to_string(l_id), login_info_from_db)){return login_info_from_db;}
   else return login_info_from_db;
 }
 
-bool LoginManager::update_Linfo_by_l_id(int l_id, std::string set_attr){
+bool LoginManageFuncs::update_Linfo_by_l_id(int l_id, std::string set_attr){
   return l_dao->update(set_attr, "l_id = "+l_id);
 }
 //=======================================
-//============CustomerManager============
-CustomerManager::CustomerManager(std::string filename)
+
+
+//============CustomerManageFuncs============
+CustomerManageFuncs::CustomerManageFuncs(std::string filename)
 {
   std::ifstream db_conf(filename.c_str(), std::ios::in);
   std::string reading_line_buffer;
@@ -91,7 +93,7 @@ CustomerManager::CustomerManager(std::string filename)
   c_dao.reset(new CustomerInfoDao(dbname,user,password));
 }
 
-bool CustomerManager::register_Cinfo(
+bool CustomerManageFuncs::register_Cinfo(
     int l_id,
     std::string last_name,
     std::string first_name,
@@ -103,21 +105,21 @@ bool CustomerManager::register_Cinfo(
   else return false;
 }
 
-CustomerInfo CustomerManager::fetch_Cinfo_by_l_id(int l_id)
+CustomerInfo CustomerManageFuncs::fetch_Cinfo_by_l_id(int l_id)
 {
   CustomerInfo cus_info_from_db = CustomerInfo();
   if (c_dao->fetch("l_id="+std::to_string(l_id), cus_info_from_db)){return cus_info_from_db;}
   else return cus_info_from_db;
 }
 
-bool CustomerManager::update_Cinfo_by_c_id(int c_id, std::string set_attr){
+bool CustomerManageFuncs::update_Cinfo_by_c_id(int c_id, std::string set_attr){
   return c_dao->update(set_attr, "id = " + std::to_string(c_id));
 }
 
 //=========================================
 
-//============DeviceManager============
-DeviceManager::DeviceManager(std::string filename)
+//============DeviceManageFuncs============
+DeviceManageFuncs::DeviceManageFuncs(std::string filename)
 {
   std::ifstream db_conf(filename.c_str(), std::ios::in);
   std::string reading_line_buffer;
@@ -147,7 +149,7 @@ DeviceManager::DeviceManager(std::string filename)
   d_dao.reset(new DeviceInfoDao(dbname,user,password));
 }
 
-bool DeviceManager::register_Dinfo(
+bool DeviceManageFuncs::register_Dinfo(
     int c_id,
     std::string device_name,
     int default_privacy_standard,
@@ -160,22 +162,22 @@ bool DeviceManager::register_Dinfo(
   else return false;
 }
 
-DeviceInfo DeviceManager::fetch_Dinfo_by_c_id(int c_id)
+DeviceInfo DeviceManageFuncs::fetch_Dinfo_by_c_id(int c_id)
 {
   DeviceInfo dev_info_from_db = DeviceInfo();
   if (d_dao->fetch("c_id="+std::to_string(c_id), dev_info_from_db)){return dev_info_from_db;}
   else return dev_info_from_db;
 }
 
-bool DeviceManager::update_Dinfo_by_d_id(int d_id, std::string set_attr){
+bool DeviceManageFuncs::update_Dinfo_by_d_id(int d_id, std::string set_attr){
   return d_dao->update(set_attr, "id = " + std::to_string(d_id));
 }
 
 //=========================================
 
 
-//============VenderManager============
-VenderManager::VenderManager(std::string filename)
+//============VenderManageFuncs============
+VenderManageFuncs::VenderManageFuncs(std::string filename)
 {
   std::ifstream db_conf(filename.c_str(), std::ios::in);
   std::string reading_line_buffer;
@@ -203,7 +205,7 @@ VenderManager::VenderManager(std::string filename)
   v_dao.reset(new VenderInfoDao(dbname,user,password));
 }
 
-bool VenderManager::register_Vinfo(
+bool VenderManageFuncs::register_Vinfo(
     int l_id,
     std::string name,
     std::string phone_num,
@@ -213,20 +215,20 @@ bool VenderManager::register_Vinfo(
   else return false;
 }
 
-VenderInfo VenderManager::fetch_Vinfo_by_l_id(int l_id)
+VenderInfo VenderManageFuncs::fetch_Vinfo_by_l_id(int l_id)
 {
   VenderInfo ven_info_from_db = VenderInfo();
   if (v_dao->fetch("l_id="+std::to_string(l_id), ven_info_from_db)){return ven_info_from_db;}
   else return ven_info_from_db;
 }
 
-bool VenderManager::update_Vinfo_by_v_id(int v_id, std::string set_attr){
+bool VenderManageFuncs::update_Vinfo_by_v_id(int v_id, std::string set_attr){
   return v_dao->update(set_attr, "id = " + std::to_string(v_id));
 }
 //=========================================
 
-//============ServiceManager============
-ServiceManager::ServiceManager(std::string filename)
+//============ServiceManageFuncs============
+ServiceManageFuncs::ServiceManageFuncs(std::string filename)
 {
   std::ifstream db_conf(filename.c_str(), std::ios::in);
   std::string reading_line_buffer;
@@ -256,7 +258,7 @@ ServiceManager::ServiceManager(std::string filename)
   s_dao.reset(new ServiceInfoDao(dbname,user,password));
 }
 
-bool ServiceManager::register_Sinfo(
+bool ServiceManageFuncs::register_Sinfo(
     int v_id,
     std::string service_name,
     int required_privacy_standard,
@@ -267,23 +269,86 @@ bool ServiceManager::register_Sinfo(
   else return false;
 }
 
-ServiceInfo ServiceManager::fetch_Sinfo_by_v_id(int v_id)
+ServiceInfo ServiceManageFuncs::fetch_Sinfo_by_v_id(int v_id)
 {
   ServiceInfo ser_info_from_db = ServiceInfo();
   if (s_dao->fetch("v_id="+std::to_string(v_id), ser_info_from_db)){return ser_info_from_db;}
   else return ser_info_from_db;
 }
 
-bool ServiceManager::update_Sinfo_by_s_id(int s_id, std::string set_attr){
+bool ServiceManageFuncs::update_Sinfo_by_s_id(int s_id, std::string set_attr){
   return s_dao->update(set_attr, "id = " + std::to_string(s_id));
 }
 
 //=========================================
 
 
+//==========RelationshipManageFuncs============
+RelationshipManageFuncs::RelationshipManageFuncs(std::string filename)
+{
+  std::ifstream db_conf(filename.c_str(), std::ios::in);
+  std::string reading_line_buffer;
+  std::vector<std::string> conf;
+  std::string dbname;
+  std::string user;
+  std::string password;
+
+  while(std::getline(db_conf, reading_line_buffer)){
+    std::cout<<reading_line_buffer<<std::endl;
+    std::string separated_string_buffer;
+    std::istringstream line_separater(reading_line_buffer);
+    while(std::getline(line_separater, separated_string_buffer, '=')){
+      std::istringstream separated_word(separated_string_buffer);
+      std::string tmp_conf;
+      separated_word>>tmp_conf;
+      conf.push_back(tmp_conf);
+    }
+  }
+
+  for(std::vector<std::string>::iterator itr = conf.begin(); itr!=conf.end(); itr++){
+    if(*itr=="dbname")dbname=*(itr+1);
+    else if(*itr=="username")user=*(itr+1);
+    else if(*itr=="password")password=*(itr+1);
+  }
+
+  r_dao.reset(new RelationshipDao(dbname,user,password));
+}
+
+bool RelationshipManageFuncs::register_Relation(
+    int d_id,
+    int s_id,
+    ANONYMITYMETHOD anonymity_method,
+    int privacy_standard,
+    int interval,
+    std::string location)
+{
+  if(r_dao->put(d_id, s_id, anonymity_method, privacy_standard, interval, location))return true;
+  else return false;
+}
+
+Relationship RelationshipManageFuncs::fetch_Relation_by_d_id(int d_id)
+{
+  Relationship relation_from_db = Relationship();
+  if (r_dao->fetch("d_id="+std::to_string(d_id), relation_from_db)){return relation_from_db;}
+  else return relation_from_db;
+}
+
+Relationship RelationshipManageFuncs::fetch_Relation_by_s_id(int s_id)
+{
+  Relationship relation_from_db = Relationship();
+  if (r_dao->fetch("s_id="+std::to_string(s_id), relation_from_db)){return relation_from_db;}
+  else return relation_from_db;
+}
+
+bool RelationshipManageFuncs::update_Relation_by_r_id(int r_id, std::string set_attr){
+  return r_dao->update(set_attr, "id = " + std::to_string(r_id));
+}
+
+
 int main(){
-  ServiceManager s("db.conf");
-  if(s.update_Sinfo_by_s_id(1, "service_name='http://www.alter_service1'"))std::cout<<"ok\n";
+  RelationshipManageFuncs r("db.conf");
+  if(r.update_Relation_by_r_id(1, "anonymity_method=1"))std::cout<<"ok\n";
   else std::cout<<"out\n";
+
   return 0;
 }
